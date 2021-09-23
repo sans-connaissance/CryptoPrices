3795b3ecd42fe3d1154dc7b92157a18af85fd1cf
sans-connaissance <davemalicke@gmail.com>
2021-09-23 14:44:57 -0400

    Initial Commit

---
+++ CryptoPrices/CryptoPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesApp.swift
+//  CryptoPrices
+//
+//  Created by David Malicke on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
